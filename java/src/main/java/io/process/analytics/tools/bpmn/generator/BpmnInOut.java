592a27e1ab978d6bbf34e0db242bf9a0cefb9d4e
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-10 00:32:35 +0200

    WIP: bpmn source parsing

---
+++ java/src/main/java/io/process/analytics/tools/bpmn/generator/BpmnInOut.java
@@ -0,0 +1,22 @@
+package io.process.analytics.tools.bpmn.generator;
+
+import io.process.analytics.tools.bpmn.generator.internal.XmlParser;
+
+public class BpmnInOut {
+
+    private final XmlParser xmlParser;
+
+    public BpmnInOut(XmlParser xmlParser) {
+        this.xmlParser = xmlParser;
+    }
+
+    public void readFromBpmn(String xml) {
+
+    }
+
+    public void writeToBpmn() {
+
+    }
+
+
+}
