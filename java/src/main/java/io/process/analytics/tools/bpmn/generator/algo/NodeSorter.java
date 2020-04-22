5bedb9b4f96ac9c45ef2dd564b59b88b94f6f8ef
Baptiste Mesta <baptiste.mesta@gmail.com>
2020-04-22 17:04:17 +0200

    First implementation of the sorting algorithm (#4)
    This algorithm sort elements of a graph in topological order.
    It supports graphs with loops.
    
    This will be used to layout elements of a BPMN diagram
    
    Co-authored-by: Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>

---
+++ java/src/main/java/io/process/analytics/tools/bpmn/generator/algo/NodeSorter.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2020 Bonitasoft S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.process.analytics.tools.bpmn.generator.algo;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import io.process.analytics.tools.bpmn.generator.model.Diagram;
+import io.process.analytics.tools.bpmn.generator.model.Edge;
+import io.process.analytics.tools.bpmn.generator.model.Shape;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+
+/**
+ * Sort nodes of a diagram in topological order.
+ *
+ * The algorithm also handles cycle.
+ *
+ */
+public class NodeSorter {
+
+
+    /**
+     * sort nodes of a diagram in topological order
+     *
+     * @param diagram to sort
+     * @return a diagram with same nodes but sorted
+     */
+    Diagram sort(Diagram diagram) {
+        Set<Shape> nodesToSort = new HashSet<>(diagram.getShapes());
+        Set<Edge> edges = new HashSet<>(diagram.getEdges());
+        List<Join> joins = findAllJoins(nodesToSort, edges);
+
+        Diagram.DiagramBuilder sortedDiagram = Diagram.builder();
+
+        while (!nodesToSort.isEmpty()) {
+            Set<Shape> startShapes = getStartNodes(nodesToSort, edges);
+            if (!startShapes.isEmpty()) {
+                for (Shape startShape : startShapes) {
+                    nodesToSort.remove(startShape);
+                    sortedDiagram.shape(startShape);
+                    edges = removeEdgesStartingWithNode(edges, startShape);
+                    joins.stream().filter(j -> j.isJoining(startShape)).forEach(Join::markProcessed);
+                }
+            } else {
+                //retrieve a join that we "entered", i.e. we processed an element that has an edge going to this join.
+                Join join = getAJoinThatWasProcessed(joins);
+                //revert all edges from this join to remove the cycle
+                edges = edges.stream().map(edge -> {
+                    if (join.contains(edge)) {
+                        //revert the edge
+                        return new Edge(edge.getTo(), edge.getFrom());
+                    } else {
+                        return edge;
+                    }
+                }).collect(Collectors.toSet());
+            }
+        }
+        return sortedDiagram.edges(diagram.getEdges()).build();
+    }
+
+    private Join getAJoinThatWasProcessed(List<Join> joins) {
+        // we should always have a join that was processed when we don't have a start node
+        return joins.stream().filter(j -> j.wasProcessed).findFirst().get();
+    }
+
+    private Set<Edge> removeEdgesStartingWithNode(Set<Edge> edges, Shape startShape) {
+        return edges.stream().filter(e -> e.getFrom() != startShape.getUuid()).collect(Collectors.toSet());
+    }
+
+    private Set<Shape> getStartNodes(Set<Shape> nodesToSort, Set<Edge> edges) {
+        return nodesToSort.stream()
+                .filter(n -> hasNoIncomingLink(n, edges))
+                .collect(Collectors.toSet());
+    }
+
+    private List<Join> findAllJoins(Set<Shape> shapes, Set<Edge> edges) {
+        //get the nodes that are "join"
+        return shapes.stream().map(n -> Join.builder().to(n))
+                .map(j -> j.incomings(edges.stream().filter(e -> e.getTo() == j.to.getUuid()).map(Edge::getFrom).collect(Collectors.toList())))
+                .map(Join.JoinBuilder::build)
+                //keep only join if there is more than 1 edge incoming
+                .filter(j -> j.incomings.size() > 1)
+                .collect(Collectors.toList());
+
+    }
+
+
+    private boolean hasNoIncomingLink(Shape m, Set<Edge> edges) {
+        return edges.stream().noneMatch(e -> e.getTo() == m.getUuid());
+    }
+
+    @Data
+    @Builder
+    private static class Join {
+        @Singular
+        private Set<UUID> incomings;
+        private final Shape to;
+        private boolean wasProcessed;
+
+        boolean isJoining(Shape shape) {
+            return incomings.contains(shape.getUuid());
+        }
+
+        void markProcessed() {
+            setWasProcessed(true);
+        }
+
+        boolean contains(Edge edge) {
+            return to.getUuid().equals(edge.getTo()) && incomings.contains(edge.getFrom());
+        }
+
+    }
+
+
+}
---
+++ java/src/test/java/io/process/analytics/tools/bpmn/generator/algo/NodeSorterTest.java
@@ -0,0 +1,111 @@
+package io.process.analytics.tools.bpmn.generator.algo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.process.analytics.tools.bpmn.generator.model.Diagram;
+import io.process.analytics.tools.bpmn.generator.model.Edge;
+import io.process.analytics.tools.bpmn.generator.model.Shape;
+import org.junit.jupiter.api.Test;
+
+class NodeSorterTest {
+
+    private NodeSorter nodeSorter = new NodeSorter();
+
+    Shape start = new Shape("start");
+    Shape step1 = new Shape("step1");
+    Shape step2 = new Shape("step2");
+    Shape step3 = new Shape("step3");
+    Shape step4 = new Shape("step4");
+    Shape step5 = new Shape("step5");
+
+    @Test
+    void should_sort_2_shapes() {
+        Diagram diagram = Diagram.builder()
+                .shape(step1)
+                .shape(start)
+                .edge(edge(start, step1))
+                .build();
+
+        Diagram sorted = nodeSorter.sort(diagram);
+
+        assertThat(sorted.getShapes()).containsExactly(start, step1);
+    }
+    @Test
+    void should_sort_3_shapes() {
+        Diagram diagram = Diagram.builder()
+                .shape(step1)
+                .shape(step2)
+                .shape(start)
+                .edge(edge(start, step1))
+                .edge(edge(step1, step2))
+                .build();
+
+        Diagram sorted = nodeSorter.sort(diagram);
+
+        assertThat(sorted.getShapes()).containsExactly(start, step1, step2);
+    }
+
+    private Edge edge(Shape step1, Shape step2) {
+        return new Edge(step1.getUuid(), step2.getUuid());
+    }
+
+    @Test
+    void should_sort_shapes_with_split_and_join() {
+        // there is 2 branches: step2 and step3
+        //
+        // start --> step1 ---------> step 2
+        //             \               \
+        //              --> step 3 -->  step 4 -->  step 5
+
+        Diagram diagram = Diagram.builder()
+                .shape(step1)
+                .shape(step3)
+                .shape(step2)
+                .shape(step5)
+                .shape(step4)
+                .shape(start)
+                .edge(edge(start, step1))
+                .edge(edge(step1, step2))
+                .edge(edge(step1, step3))
+                .edge(edge(step3, step4))
+                .edge(edge(step2, step4))
+                .edge(edge(step4, step5))
+                .build();
+
+        Diagram sorted = nodeSorter.sort(diagram);
+
+        assertThat(sorted.getShapes()).startsWith(start, step1);
+        assertThat(sorted.getShapes()).contains(step2, step3);
+        assertThat(sorted.getShapes()).endsWith(step4, step5);
+    }
+
+    @Test
+    void should_sort_shapes_with_loop() {
+        // loop between step2, step3, step4
+        //
+        //                      -------------------------
+        //                      |                        |
+        //                      V                        |
+        // start --> step1 --> step 2 --> step 3 -->  step 4 -->  step 5
+
+        Diagram diagram = Diagram.builder()
+                .shape(step1)
+                .shape(step3)
+                .shape(step2)
+                .shape(step5)
+                .shape(step4)
+                .shape(start)
+                .edge(edge(start, step1))
+                .edge(edge(step1, step2))
+                .edge(edge(step2, step3))
+                .edge(edge(step3, step4))
+                .edge(edge(step4, step2))
+                .edge(edge(step4, step5))
+                .build();
+
+        Diagram sorted = nodeSorter.sort(diagram);
+
+        assertThat(sorted.getShapes()).containsExactly(start, step1, step2, step3, step4, step5);
+    }
+
+}
