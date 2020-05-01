c6b3647fd8cdf1c76e60e5f1ad2fa3971540aa63
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-05-01 12:47:42 +0200

    clean commented code

--- java/src/main/java/io/process/analytics/tools/bpmn/generator/export/SVGExporter.java
+++ java/src/main/java/io/process/analytics/tools/bpmn/generator/export/SVGExporter.java
@@ -21,15 +21,9 @@ import io.process.analytics.tools.bpmn.generator.converter.DisplayModelConverter
 import io.process.analytics.tools.bpmn.generator.converter.DisplayModelConverter.DisplayLabel;
 import io.process.analytics.tools.bpmn.generator.converter.DisplayModelConverter.DisplayModel;
 import io.process.analytics.tools.bpmn.generator.model.Grid;
-import io.process.analytics.tools.bpmn.generator.model.Position;
 import io.process.analytics.tools.bpmn.generator.model.SortedDiagram;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 public class SVGExporter {
-
-//    private static final int CELL_WIDTH = 200;
-//    private static final int CELL_HEIGHT = 100;
 
     private final DisplayModelConverter converter = new DisplayModelConverter();
 
@@ -64,60 +58,5 @@ public class SVGExporter {
         content.append("</svg>");
         return content.toString().getBytes();
     }
-
-//    public byte[] exportSvg(Grid grid, SortedDiagram diagram) {
-//        StringBuilder content = new StringBuilder();
-//        content.append("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"")
-//                .append(grid.width() * CELL_WIDTH)
-//                .append("\" height=\"")
-//                .append(grid.height() * CELL_HEIGHT).append("\">\n");
-//        for (Position position : grid.getPositions()) {
-//            int xOffset = position.getX() * CELL_WIDTH;
-//            int yOffset = position.getY() * CELL_HEIGHT;
-//            int nodeWidth = x(60);
-//            int nodeHeight = y(60);
-//
-//            int x = xOffset + (CELL_WIDTH - nodeWidth) / 2;
-//            int y = yOffset + (CELL_HEIGHT - nodeHeight) / 2;
-//            DisplayDimension flowNodeDimension = new DisplayDimension(x, y, nodeWidth, nodeHeight);
-//
-//            // TODO manage empty name
-//            String name = diagram.getShapes().stream().filter(n -> n.getId().equals(position.getShape())).findFirst().get().getName();
-//            int labelX = xOffset + x(50);
-//            int labelY = yOffset + y(50);
-//
-//            DisplayDimension labelDimension = new DisplayDimension(labelX, labelY, -1, -1);
-//            DisplayLabel label = new DisplayLabel(name, y(16), labelDimension);
-//
-//            DisplayFlowNode flowNode = new DisplayFlowNode(flowNodeDimension, label, y(10), y(5));
-//
-//
-//            content.append("<rect ");
-//            content.append("x=\"").append(flowNodeDimension.x).append("\" ");
-//            content.append("y=\"").append(flowNodeDimension.y).append("\" ");
-//            content.append("width=\"").append(flowNodeDimension.width).append("\" ");
-//            content.append("height=\"").append(flowNodeDimension.height).append("\" ");
-//            content.append("rx=\"").append(flowNode.rx).append("\" ");
-//            content.append("fill=\"#E3E3E3\" stroke=\"#92ADC8\" ");
-//            content.append("stroke-width=\"").append(flowNode.strokeWidth).append("\"/>\n");
-//            content.append("<text x=\"").append(labelDimension.x);
-//            content.append("\" y=\"").append(labelDimension.y);
-//            content.append("\" text-anchor=\"middle\" font-size=\"").append(label.fontSize);
-//            content.append("\" fill=\"#374962\">");
-//            content.append(label.text).append("</text>\n");
-//        }
-//        content.append("</svg>");
-//        return content.toString().getBytes();
-//    }
-
-
-
-//    private int x(int percentage) {
-//        return CELL_WIDTH * percentage / 100;
-//    }
-//
-//    private int y(int percentage) {
-//        return CELL_HEIGHT * percentage / 100;
-//    }
 
 }
