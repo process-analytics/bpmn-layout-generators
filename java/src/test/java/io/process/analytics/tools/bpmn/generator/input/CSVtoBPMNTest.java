08bd50f7afc083d631bc027dd65f749eb4312913
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-07-02 12:18:15 +0200

    test: simplify file reading

--- java/src/test/java/io/process/analytics/tools/bpmn/generator/input/CSVtoBPMNTest.java
+++ java/src/test/java/io/process/analytics/tools/bpmn/generator/input/CSVtoBPMNTest.java
@@ -5,18 +5,18 @@ import io.process.analytics.tools.bpmn.generator.internal.generated.model.TDefin
 import io.process.analytics.tools.bpmn.generator.internal.generated.model.TProcess;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
+import static io.process.analytics.tools.bpmn.generator.internal.FileUtils.fileContent;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CSVtoBPMNTest {
     @Test
     public void should_convert_csv_to_bpmn() throws IOException {
-        String edge = new String(Files.readAllBytes(Paths.get("src", "test", "resources", "csv", "PatientsProcess", "edge.csv")));
-        String node = new String(Files.readAllBytes(Paths.get("src", "test", "resources", "csv", "PatientsProcess", "node.csv")));
+        String edge = readCsvFile("src/test/resources/csv/PatientsProcess/edge.csv");
+        String node = readCsvFile("src/test/resources/csv/PatientsProcess/node.csv");
 
         TDefinitions bpmn = new CSVtoBPMN().readFromCSV(node, edge);
         Semantic semantic = new Semantic(bpmn);
@@ -27,6 +27,10 @@ class CSVtoBPMNTest {
         Semantic.BpmnElements bpmnElements = semantic.getBpmnElements(tProcess);
         assertThat(bpmnElements.getFlowNodes()).hasSize(9);
         assertThat(bpmnElements.getSequenceFlows()).hasSize(13);
+    }
 
+    // TODO the underlying method read lines then join. The CSVtoBPMN spit the string into lines! Useless work!
+    private static String readCsvFile(String csvFilePath) throws IOException {
+        return fileContent(new File(csvFilePath));
     }
 }
