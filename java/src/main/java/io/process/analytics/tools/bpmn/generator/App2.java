2e9a744a7355948abf1b5c2bcc27a50d4dff2e27
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-07-11 00:01:20 +0200

    refactor: remove duplication in the 2 main classes

--- java/src/main/java/io/process/analytics/tools/bpmn/generator/App2.java
+++ java/src/main/java/io/process/analytics/tools/bpmn/generator/App2.java
@@ -1,36 +1,38 @@
 package io.process.analytics.tools.bpmn.generator;
 
-import io.process.analytics.tools.bpmn.generator.algo.ShapeLayouter;
-import io.process.analytics.tools.bpmn.generator.algo.ShapeSorter;
-import io.process.analytics.tools.bpmn.generator.converter.AlgoToDisplayModelConverter;
-import io.process.analytics.tools.bpmn.generator.converter.BpmnToAlgoModelConverter;
-import io.process.analytics.tools.bpmn.generator.export.BPMNExporter;
-import io.process.analytics.tools.bpmn.generator.input.CSVtoBPMN;
-import io.process.analytics.tools.bpmn.generator.internal.BpmnInOut;
-import io.process.analytics.tools.bpmn.generator.internal.generated.model.TDefinitions;
-import io.process.analytics.tools.bpmn.generator.model.Diagram;
-import io.process.analytics.tools.bpmn.generator.model.Grid;
+import static io.process.analytics.tools.bpmn.generator.internal.BpmnInOut.defaultBpmnInOut;
+import static io.process.analytics.tools.bpmn.generator.internal.FileUtils.fileContent;
 
 import java.io.File;
 import java.io.IOException;
 
-import static io.process.analytics.tools.bpmn.generator.internal.FileUtils.fileContent;
+import io.process.analytics.tools.bpmn.generator.input.CSVtoBPMN;
+import io.process.analytics.tools.bpmn.generator.internal.BpmnInOut;
+import io.process.analytics.tools.bpmn.generator.internal.generated.model.TDefinitions;
+import lombok.extern.log4j.Log4j2;
 
-public class App2 {
+@Log4j2
+public class App2 extends App {
+
+    public App2(BpmnInOut bpmnInOut) {
+        super(bpmnInOut);
+    }
+
     public static void main(String[] args) throws IOException {
-
         File nodeDiscovery = new File(args[0]);
         File edgeDiscovery = new File(args[1]);
         File outputFile = new File(args[2]);
 
-        TDefinitions tDefinitions = new CSVtoBPMN().readFromCSV(fileContent(nodeDiscovery), fileContent(edgeDiscovery));
-
-        Diagram diagram = new BpmnToAlgoModelConverter().toAlgoModel(tDefinitions);
-        Diagram sortedDiagram = new ShapeSorter().sort(diagram);
-        Grid grid = new ShapeLayouter().layout(sortedDiagram);
-
-        BPMNExporter bpmnExporter = new BPMNExporter(new AlgoToDisplayModelConverter());
-        TDefinitions newDefinitions = bpmnExporter.export(tDefinitions, grid, diagram);
-        BpmnInOut.defaultBpmnInOut().writeToBpmnFile(newDefinitions, outputFile);
+        new App2(defaultBpmnInOut()).process(nodeDiscovery, edgeDiscovery, outputFile);
     }
+
+    private void process(File nodeDiscovery, File edgeDiscovery, File outputFile) throws IOException {
+        log.info("Converting CSV into internal model");
+        TDefinitions definitions = new CSVtoBPMN().readFromCSV(fileContent(nodeDiscovery), fileContent(edgeDiscovery));
+        log.info("Conversion done");
+
+        LayoutSortedDiagram layoutSortedDiagram = computeLayout(definitions);
+        exportToBpmn(layoutSortedDiagram, outputFile);
+    }
+
 }
