67536a5df462ce657876d4afc3a35c5922867d3a
Pascal GARCIA <pascal.garcia@bonitasoft.com>
2021-06-02 15:04:54 +0200

    concatenate csv file

--- R/bpmnLayoutGeneratoR/R/bpmnLayoutGeneratoR.R
+++ R/bpmnLayoutGeneratoR/R/bpmnLayoutGeneratoR.R
@@ -5,14 +5,9 @@ generateBpmnLayout <- function(flow_node, sequence_flow, outputType = "BPMN") {
   bpmnLayoutJava <- rJava::.jnew("io/process/analytics/tools/bpmn/generator/BPMNLayoutGenerator")
   type <- rJava::J(class = "io/process/analytics.tools/bpmn/generator/BPMNLayoutGenerator$ExportType", "valueOf", outputType)
 
+  flow_node_as_csv <- paste(to_csv(flow_node), collapse = "\n")
+  sequence_flow_as_csv <- paste(to_csv(sequence_flow), collapse = "\n")
 
-  flow_node_as_csv <- to_csv(flow_node)
-  sequence_flow_as_csv <- to_csv(sequence_flow)
-  writeLines("flow nodes are:\n")
-  writeLines(flow_node_as_csv)
-  writeLines("sequence flows are:")
-  writeLines(sequence_flow_as_csv)
-  writeLines(class(flow_node_as_csv))
   diagramAsString <- bpmnLayoutJava$generateLayoutFromCSV(flow_node_as_csv, sequence_flow_as_csv, type)
   return(diagramAsString)
 }
@@ -22,3 +17,7 @@ to_csv <- function(x) {
   utils::write.csv(x, connection)
   return(fn_as_csv)
 }
+
+"1,1,Add event on employee calendar,task"
+
+"1,1,1,2012"
