a6f57c4e64d6452178e0ff03651eb5b38979f687
Baptiste Mesta <baptiste.mesta@gmail.com>
2021-06-02 11:06:40 +0200

    try to call the lib

--- R/bpmnLayoutGeneratoR/R/bpmnLayoutGeneratoR.R
+++ R/bpmnLayoutGeneratoR/R/bpmnLayoutGeneratoR.R
@@ -1,6 +1,24 @@
-
-
 #' @export
-generateBpmnLayout <- function(flow_node, sequence_flow){
-  return(cat(flow_node))
+generateBpmnLayout <- function(flow_node, sequence_flow, outputType = "BPMN") {
+  rJava::.jinit()
+  rJava::.jaddClassPath("../../java/target/bpmn-layout-generator-0.1.0-SNAPSHOT-jar-with-dependencies.jar")
+  bpmnLayoutJava <- rJava::.jnew("io/process/analytics/tools/bpmn/generator/BPMNLayoutGenerator")
+  type <- rJava::J(class = "io/process/analytics.tools/bpmn/generator/BPMNLayoutGenerator$ExportType", "valueOf", outputType)
+
+
+  flow_node_as_csv <- to_csv(flow_node)
+  sequence_flow_as_csv <- to_csv(sequence_flow)
+  writeLines("flow nodes are:\n")
+  writeLines(flow_node_as_csv)
+  writeLines("sequence flows are:")
+  writeLines(sequence_flow_as_csv)
+  writeLines(class(flow_node_as_csv))
+  diagramAsString <- bpmnLayoutJava$generateLayoutFromCSV(flow_node_as_csv, sequence_flow_as_csv, type)
+  return(diagramAsString)
+}
+
+to_csv <- function(x) {
+  connection <- textConnection("fn_as_csv", "w", local = TRUE)
+  utils::write.csv(x, connection)
+  return(fn_as_csv)
 }
