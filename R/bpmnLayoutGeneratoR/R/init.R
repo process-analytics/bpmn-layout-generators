44bafdafd4d0950c5e7d16a97afca86a1b01464f
Baptiste Mesta <baptiste.mesta@gmail.com>
2021-06-02 17:08:29 +0200

    try 4

--- R/bpmnLayoutGeneratoR/R/init.R
+++ R/bpmnLayoutGeneratoR/R/init.R
@@ -1,5 +1,8 @@
 .onLoad <- function(libname, pkgname) {
-  cat("Calling on load")
+  writeLines("Calling on load")
+  writeLines(file.path(getwd()))
+  writeLines(dir(file.path(getwd())))
+  writeLines(dir(file.path(getwd(), "java")))
   rJava::.jpackage(pkgname, lib.loc = libname)
   rJava::.jaddClassPath(dir(file.path(getwd(), "java"), full.names = TRUE))
 }
